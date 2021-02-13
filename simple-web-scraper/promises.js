f171f87543584073f2ed29c89874313a8f2d1520
jrtnq514 <burggamer.jt@gmail.com>
2021-02-13 13:11:27 -0600

    Scraping sample

---
+++ simple-web-scraper/promises.js
@@ -0,0 +1,23 @@
+const got = require('got');
+const cheerio = require('cheerio');
+
+const URL_TO_SCRAPE = 'https://reddit.com';
+
+// Fetch the webpage
+got(URL_TO_SCRAPE)
+  .then(response => {
+    // Load the page into cheerio for parsing
+    const $ = cheerio.load(response.body);
+
+    let titles = [];
+
+    $('a h3').each((index, element) => {
+      titles.push($(element).text());
+    });
+
+    console.log(titles);
+
+  })
+  .catch(error => {
+    console.log(error);
+  });
